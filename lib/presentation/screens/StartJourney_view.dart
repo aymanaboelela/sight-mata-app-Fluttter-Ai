36156215e25605c13a44fff6c15fc11284e41ad9
aymanaboelela <aymanaboelela222@gmail.com>
2025-02-10 21:47:28 +0200

    refactor StartJourney view layout for improved structure and user experience; add VSCode settings for project configuration

--- lib/presentation/screens/StartJourney_view.dart
+++ lib/presentation/screens/StartJourney_view.dart
@@ -14,60 +14,43 @@ class StartJourney extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        backgroundColor: AppColors.primaryBlueColor,
-        leading: IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-         icon:Icon(Icons.arrow_back_ios,color: Colors.white,) ),
-        title: const Row(
-          children: [
-            CircleAvatar(
-              radius: 20,
-              backgroundColor: Color(0xff94B2C8),
-              child: Icon(
-                Icons.person_outlined,
-                color: Color(0xff7897AD),
-                size: 25,
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              "Hi, Basmala.",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontFamily: 'Bahnschrift',
-                  fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
-      ),
-      body:  Center(
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             GestureDetector(
-              onTap: (){
-                //  GoRouter.of(context)
-                //                   .pushReplacement(AppRouter.kDistanceOff);
-              },
+              onTap: onTap,
               child: const CircleAvatar(
                 radius: 40,
                 backgroundColor: Color(0xffADADAD),
-                child: Icon(Icons.add,color: Colors.white,size: 75,),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 75,
+                ),
               ),
             ),
-            const SizedBox(height: 15,),
-            const Text("Start Your Journey",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 17,color: Colors.black),),
-           const  SizedBox(height: 5,)
-            ,const Text("Start by adding your first to keep you informed\n about the  location and alerts for thier safety.",
-            style:TextStyle(color: Color(0xffADADAD),) ,)
-            ],
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Start Your Journey",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 17,
+                  color: Colors.black),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "Start by adding your first to keep you informed\n about the  location and alerts for thier safety.",
+              style: TextStyle(
+                color: Color(0xffADADAD),
+              ),
+            )
+          ],
         ),
       ),
     );
