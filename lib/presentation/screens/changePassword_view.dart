0945111213f2c35ba369f6682f2d40d349565bbd
ahmed-abdelgawad-123 <ahmedabdelgawadhassan11@gmail.com>
2025-02-14 10:15:59 +0200

    solve some bugs in change pass view

--- lib/presentation/screens/changePassword_view.dart
+++ lib/presentation/screens/changePassword_view.dart
@@ -10,43 +10,45 @@ class ChangepasswordView extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 10, left: 20, right: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Change Password",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const MyPfrofileTextFormField(
-              label: "Old Password",
-              hintText: "Enter Old Password",
-            ),
-            const MyPfrofileTextFormField(
-              label: "New Password",
-              hintText: 'Enter New Password',
-            ),
-            const MyPfrofileTextFormField(
-              label: "Confirm New Password",
-              hintText: "Confirm New Password",
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 250),
-              child: Savebutton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Change Password",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 5,
+              ),
+              const MyPfrofileTextFormField(
+                label: "Old Password",
+                hintText: "Enter Old Password",
+              ),
+              const MyPfrofileTextFormField(
+                label: "New Password",
+                hintText: 'Enter New Password',
+              ),
+              const MyPfrofileTextFormField(
+                label: "Confirm New Password",
+                hintText: "Confirm New Password",
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 250),
+                child: Savebutton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
